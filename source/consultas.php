41050908ac577dac6bd73b0e20fc7158e3098fb2
MUDCROSS1507 <43319453+MUDCROSS1507@users.noreply.github.com>
2019-03-08 12:13:51 -0600

    php y partials de paco acabdos

--- source/consultas.php
+++ source/consultas.php
@@ -1,4 +1,4 @@
 <?php include ("views/_header.html") ?>
 <?php session_start(); ?>
-<?php include ("/adminstracion/partials/_formulario_reportes.html") ?> 
+<?php include ("adminstracion/partials/_formulario_consultas.html") ?> 
 <?php include ("views/_footer.html") ?> 
