9dc251a7c78286854c8693b256e4ff4a74228872
A01209032 <A01209032@itesm.mx>
2019-03-10 11:05:03 -0600

    test

--- source/registro.php
+++ source/registro.php
@@ -1,9 +1,25 @@
 <?php
-
+  session_start();
   require_once("util.php");
   header_html();
-  require_once("registroAsistencia.php");
 
+  switch ($_SESSION['departamento']) {
+    case 'asistencias':
+      require_once("registroAsistencia.php");
+      break;
+
+    case 'dispensario':
+      require_once("registroDisepnsario.php");
+      break;
+
+    case 'porteria':
+      require_once("registroPorteria.php");
+        break;
+
+    default:
+      // code...
+      break;
+  }
 
 
 
