249ece9cb85714041aff77528f92e8f86b806a95
jjol <jjoulk@gmail.com>
2019-03-10 09:04:40 -0600

    Validar Password en PHP V1

--- source/login.php
+++ source/login.php
@@ -1,9 +1,42 @@
 <?php 
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['user'])) {
+
+	// include('administracion/util.php');
+
+	// $conn = conectDb();
+
+	// $query = "SELECT idDepartamento,nombre,constrasenia FROM departamentos WHERE nombre = ".$_POST['user']." AND contrasenia = ".$_POST['pass'];
+
+	// $res = mysqli_query($conn, $query);
+
+	// if (mysqli_num_rows($res) > 0) {
+		// User Validated!
+
+		if ($_POST['user'] == "administrador" &&
+			$_POST['pass'] == "0,4,1,4") {
+			header("Location: /admin.php");
+		} else if ($_POST['pass'] == "0,3,2,0,0" ||
+				   $_POST['pass'] == "3,2,1,0,4" ||
+				   $_POST['user'] == "porteria") {
+			header("Location: /registro.php");
+		}
+		else {
+			$error = "Contraseña No Correcta!";
+			include("views/_header_login.html");
+			include("login_view.php");
+		}
+
+	// } else {
+		// $error = "Contraseña No Correcta!";
+		// include("login.html");
+	// }
+
+	// closeDb($conn);
 
 } else {
-
+	include("views/_header_login.html");
+	include("login_view.php");
 }
 
 ?>
--- source/util.php
+++ source/util.php
@@ -8,5 +8,16 @@ function info($mensaje) {
    $mensaje = '<div class="alert alert-primary" role="alert">'.$mensaje.'</div>';
    echo $mensaje;
 }
+
+
+function authSession($targetURL) {
+	session_start();
+
+	if (isset($_SESSION['user'])) {
+		return true;
+	}
+
+
+}
     
 ?>
