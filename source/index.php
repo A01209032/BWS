7131056be55bc2d8bd97ade5a118715b604239c1
jjol <jjoulk@gmail.com>
2019-03-22 11:02:30 -0600

    Nav Algo

--- source/index.php
+++ source/index.php
@@ -1,4 +1,19 @@
 <?php
+  session_start();
+
+  if (isset($_SESSION['departamento']) && !empty($_SESSION['departamento']))
+  {
+  	// Send To Corresponding Dashboard
+  	if ($_SESSION['departamento'] == "administrador") {
+  		header("Location: ".frombase("admin.php"));
+  	} else {
+  		header("Location: ".frombase("registro.php"));
+  	}
+  }
+  else
+  {
+  	header('Location: ');
+  }
   include("views/_header_login.html");
   include("login_view.php");
  ?>
