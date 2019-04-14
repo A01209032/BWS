1a4db012219640bea732e193275f0d448df7f57d
Tony <A01209245@itesm.mx>
2019-04-13 22:28:15 -0500

    Corregir validacion de contraseña nuevas

--- source/administracion/controlador/guardar_contrasena.php
+++ source/administracion/controlador/guardar_contrasena.php
@@ -1,31 +1,51 @@
 <?php
     //session_start();
     require_once("../modelo/util.php");
-    //include("../views/_header_carpetas.html");
-        
+
     //Validación de datos
     if(isset($_POST["contrasenaNueva"]) && isset($_POST["contrasenaNueva"]) != ""){
         $password=htmlspecialchars($_POST["contrasenaNueva"]);
         $id=htmlspecialchars($_POST["id"]);
-        modifyContraseniaById($id,$password);
-        //echo '<script language="javascript">';
-        //echo 'alert("Se edito la contraseña de manera correcta);window.location="cuentas.php";';
-        //echo '</script>';
-        //Se cargaron los datos
-        //var_dump($_POST);
-        //echo '<script language="javascript">';
-        //echo 'alert("Se edito el voluntario");
-        //echo 'window.location="cuentas.php";';
-        //echo '</script>';
-        header('Location: ../cuentas.php');
+        $arr = explode(",", $password);
+        $numero=True;
+        $valores=True;
+        $tamanio=count($arr);
+        for($i=0; $i < $tamanio; $i++) { 
+            if (!is_numeric($arr[$i])) {
+                    $numero=False;
+            }else{
+                if(($arr[$i]<0) || ($arr[$i]>4)){
+                    $valores=False;
+                }
+            }
+        }
+        if ($numero==True) {
+            if($valores==True){
+                modifyContraseniaById($id,$password);
+                echo '<script language="javascript">';
+                echo 'alert("¡La contraseña se cambio con éxito!");';
+                echo 'window.location="../cuentas.php";';
+                echo '</script>';
+            }else{
+                echo '<script language="javascript">';
+                echo 'alert("Error: Ingrese números con valores del 0 al 4");';
+                echo 'window.location="../cuentas.php";';
+                echo '</script>';
+            }
+            
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("¡Error! Porfavor ingrese adecuadamente los datos");';
+            echo 'window.location="../cuentas.php";';
+            echo '</script>';
+        }
+        //header('Location: ../cuentas.php');
+
     }else{
         //Error al cargar las datos
         echo '<script language="javascript">';
-        echo 'alert("No se encontro el registro de volutario");';
+        echo 'alert("¡Error! Porfavor inserte los datos");';
         echo '</script>';
         var_dump($_POST);
     }
-    //include("../views/_footer.html");
-    
-  
 ?>
