7101a3be0d22df5fcf4e2ecaea2acf7f7e0770a4
Samuel Ramirez <a01209032@itesm.mx>
2019-04-17 18:39:22 +0000

    sql updated

--- source/registros/util.php
+++ source/registros/util.php
@@ -109,7 +109,7 @@ function addRegistro($departamento,$paciente,$asistente,$fecha,$tipo,$observacio
 function addPaciente($nombre,$enfermedad,$direccion,$telefono,$celular,$fechaNacimiento,$sexo,$religion,$nivel){
     $con= conectDb();
     //echo '<script type="text/javascript">','alert("'.$nombre.' '.$enfermedad.' '.$direccion.' '.$telefono.' '.$celular.' '.$fechaNacimiento.' '.$sexo.' '.$religion.' '.$nivel.'");','</script>';
-    $sql ="INSERT INTO `pacientes`(`idEnfermedad`,`Nombre`, `FechadeNacimiento`, `Sexo`, `Activo`, `Direccion`, `Telefono`, `Celular`,`Religion`,`NivelEconomico`) values($enfermedad,'$nombre','$fechaNacimiento','$sexo',1,'$direccion',$telefono,$celular,'$religion','$nivel')";
+    $sql ="INSERT INTO `pacientes`(`IdEnfermedad`, `Nombre`, `FechadeNacimiento`, `Sexo`, `Activo`, `Direccion`, `Telefono`, `Celular`, `Religion`, `NivelEconomico`) values($enfermedad,'$nombre','$fechaNacimiento','$sexo',1,'$direccion','$telefono','$celular','$religion','$nivel')";
     $result = mysqli_query($con,$sql);
     closeDb($con);
     return $result;
