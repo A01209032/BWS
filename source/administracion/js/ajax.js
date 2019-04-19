8d980fb497446d83a6d9d3dbab136ab28df985df
MUDCROSS1507 <43319453+MUDCROSS1507@users.noreply.github.com>
2019-04-18 22:29:00 -0500

    Reporte MP listo

--- source/administracion/js/ajax.js
+++ source/administracion/js/ajax.js
@@ -8,6 +8,23 @@ function getRequestObject(){
         return (null);
     }
 }
+function sendRequest(){
+
+  let NUEVO_CODIGO = 1;
+   if (NUEVO_CODIGO) {
+        $.post( "controlador/consulta_tabla.php", { date: document.getElementById('date').value ,depa: document.getElementById('depa').value })
+          .done(function( data ) {
+              var ajaxResponse = document.getElementById('ajaxResponse');
+              ajaxResponse.innerHTML = data;
+              ajaxResponse.style.visibility = "visible";
+          });
+   } else {
+       
+   }
+   
+}
+
+
 function sendRequest3(){
 
   let NUEVO_CODIGO = 1;
@@ -25,39 +42,6 @@ function sendRequest3(){
        
    }
    
-}
-
-function sendRequest(){
-
-  let NUEVO_CODIGO = 1;
-   if (NUEVO_CODIGO) {
-        $.post( "controlador/consulta_tabla.php", { date: document.getElementById('date').value ,depa: document.getElementById('depa').value })
-          .done(function( data ) {
-              var ajaxResponse = document.getElementById('ajaxResponse');
-              ajaxResponse.innerHTML = data;
-              ajaxResponse.style.visibility = "visible";
-          });
-   } else {
-       
-   }
-   
-}
-function sendRequest2(){
-
-  let NUEVO_CODIGO = 1;
-   
-   if (NUEVO_CODIGO) {
-       
-       $.post( "controlador/reporte_tabla.php", { date: document.getElementById('date').value ,date2: document.getElementById('date2').value })
-          .done(function( data ) {
-              var ajaxResponse2 = document.getElementById('ajaxResponse2');
-              ajaxResponse2.innerHTML = data;
-              ajaxResponse2.style.visibility = "visible";
-          });
-   } else {
-     
-       
-   }
 }
 
 
@@ -79,6 +63,62 @@ function sendRequest4(){
    }
    
 }
+//reportes
+function sendRequest2(){
+
+  let NUEVO_CODIGO = 1;
+   
+   if (NUEVO_CODIGO) {
+       
+       $.post( "controlador/reporte_tabla.php", { date: document.getElementById('date').value ,date2: document.getElementById('date2').value })
+          .done(function( data ) {
+              var ajaxResponse2 = document.getElementById('ajaxResponse2');
+              ajaxResponse2.innerHTML = data;
+              ajaxResponse2.style.visibility = "visible";
+          });
+   } else {
+     
+       
+   }
+}
+function sendRequest5(){
+
+  let NUEVO_CODIGO = 1;
+   
+   if (NUEVO_CODIGO) {
+       
+       $.get( "controlador/verificarv.php", { pattern2: document.getElementById('userInput2').value  })
+          .done(function( data ) {
+              var ajaxResponse3= document.getElementById('ajaxResponse4');
+              ajaxResponse3.innerHTML = data;
+              ajaxResponse3.style.visibility = "visible";
+          });
+   } else {
+     
+       
+   }
+   
+}
+function sendRequest6(){
+
+  let NUEVO_CODIGO = 1;
+   
+   if (NUEVO_CODIGO) {
+       
+       $.get( "controlador/verificarv.php", { pattern2: document.getElementById('userInput2').value  })
+          .done(function( data ) {
+              var ajaxResponse3= document.getElementById('ajaxResponse4');
+              ajaxResponse3.innerHTML = data;
+              ajaxResponse3.style.visibility = "visible";
+          });
+   } else {
+     
+       
+   }
+   
+}
+
+//
 var auxiliar;
 var auxiliar2;
 function getval(first){
@@ -87,69 +127,6 @@ function getval(first){
 function getval2(second){
     auxiliar2=second;
 }
-$(document).ready(function(){  
-
-  window.onload=requestCuentas();
-
-  function requestCuentas(){
-    $.get( "controlador/obtenerCuentas.php")
-          .done(function( data ) {
-              var ajaxResponse = document.getElementById('reporteCuentas');
-              ajaxResponse.innerHTML = data;
-              ajaxResponse.style.visibility = "visible";
-              insertar();
-          });
-  }
-
-  function cambiarContrasena() {
-  var retVal = confirm("¿Esta seguro que desea cambiar la contraseña?");
-    if( retVal == true ) {
-      //document.write ("User wants to continue!");
-      return true;
-    } else {
-      //document.write ("User does not want to continue!");
-      return false;
-    }
-  }
-
-  function insertar(){
-    $('form.ajax').on("submit", function(event){  
-    event.preventDefault(); 
-    if(cambiarContrasena()){
-      var that=$(this),
-        url = 'controlador/guardar_contrasena.php',
-        type=that.attr('method'),
-        data={};
-      that.find('[name]').each(function(index,value){
-        var eso=$(this);
-        name=eso.attr('name'),
-        value=eso.val();
-
-        data[name]=value;
-      });  
-      //var boton=data[id];
-      //boton='#'+boton;
-      $.ajax({
-        url: url,
-        type:type,
-        data: data,
-        /*
-        beforeSend:function(){
-          echo (boton);
-          $(boton).val("Insertando");  
-        }, */
-        success: function(data) {
-          alert(data);
-          that[0].reset();
-          requestCuentas();
-        }
-      });
-    }
-      return false;
-  });
-  }
-  
-});
 
 
 function selectValue() {
